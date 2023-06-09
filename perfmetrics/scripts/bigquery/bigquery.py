a2080391499f09b60123505932cf38424ac397e9
RuchikaSharma-8 <2020csb1119@iitrpr.ac.in>
2023-06-09 10:12:13 +0000

    Removing some FIO jobs for testing

--- perfmetrics/scripts/bigquery/bigquery.py
+++ perfmetrics/scripts/bigquery/bigquery.py
@@ -3,26 +3,19 @@ from google.cloud import bigquery
 # Instantiate a BigQuery client
 client = bigquery.Client()
 
-id = 0
-
-def get_id():
-  id = id + 1
-  return id
-
 query_create_table_experiment_configuration = """
     CREATE OR REPLACE TABLE gcsfuse-intern-project-2023.performance_metrics.experiment_configuration(
       configuration_id INT64, 
       gcsfuse_flags STRING, 
       branch STRING, 
-      end_date TIMESTAMP, 
-      vm_instance STRING, 
+      end_date TIMESTAMP,  
       PRIMARY KEY (gcsfuse_flags, branch, end_date) NOT ENFORCED
-    ) OPTIONS (description = 'Table for storing Job Configurations and respective VM    instance name on which the job was run');
+    ) OPTIONS (description = 'Table for storing Job Configurations and respective VM instance name on which the job was run');
 """
 query_create_table_fio_metrics = """
     CREATE OR REPLACE TABLE gcsfuse-intern-project-2023.performance_metrics.fio_metrics(
       configuration_id INT64, 
-      read_or_write STRING, 
+      test_type STRING, 
       num_threads INT64, 
       file_size_kb INT64, 
       start_time INT64, 
@@ -43,9 +36,7 @@ query_create_table_fio_metrics = """
 query_create_table_vm_metrics = """
     CREATE OR REPLACE TABLE gcsfuse-intern-project-2023.performance_metrics.vm_metrics(
       configuration_id INT64, 
-      gcsfuse_flags STRING, 
-      branch STRING, 
-      end_time_vm INT64, 
+      end_time_vm INT64
       end_time INT64, 
       cpu_utilization_peak_percentage FLOAT64, 
       cpu_utilization_mean_percentage FLOAT64, 
@@ -64,12 +55,10 @@ query_create_table_vm_metrics = """
       FOREIGN KEY(configuration_id) REFERENCES performance_metrics.experiment_configuration (configuration_id) NOT ENFORCED
     ) OPTIONS (description = 'Table for storing VM metrics extracted from periodic performance load testing');
 """
-query_create_table_ls_metrics_gcsfuse = """
+query_create_table_ls_metrics = """
     CREATE OR REPLACE TABLE gcsfuse-intern-project-2023.performance_metrics.ls_metrics_gcsfuse(
-      configuration_id INT64, 
-      gcsfuse_flags STRING, 
-      branch STRING, 
-      test_description STRING, 
+      configuration_id INT64,
+      test_type STRING, 
       command STRING, 
       num_files INT64, 
       num_folder INT64, 
@@ -82,10 +71,6 @@ query_create_table_ls_metrics_gcsfuse = """
       percentile_latency_50 FLOAT64, 
       percentile_latency_90 FLOAT64, 
       percentile_latency_95 FLOAT64, 
-      percentile_latency_98 FLOAT64, 
-      percentile_latency_99 FLOAT64, 
-      percentile_latency_99pt5 FLOAT64, 
-      percentile_latency_99pt9 FLOAT64, 
       percentile_latency_100 FLOAT64, 
       cpu_utilization_peak_percentage FLOAT64, 
       cpu_utilization_mean_percentage FLOAT64,
@@ -93,48 +78,18 @@ query_create_table_ls_metrics_gcsfuse = """
       FOREIGN KEY(configuration_id) REFERENCES performance_metrics.experiment_configuration (configuration_id) NOT ENFORCED
     ) OPTIONS (description = 'Table for storing GCSFUSE metrics extracted from listing benchmark tests');
 """
-query_create_table_ls_metrics_persistent_disk = """
-    CREATE OR REPLACE TABLE gcsfuse-intern-project-2023.performance_metrics.ls_metrics_persistent_disk(
-      configuration_id INT64, 
-      gcsfuse_flags STRING, 
-      branch STRING, 
-      test_description STRING, 
-      command STRING, 
-      num_files INT64, 
-      num_folder INT64, 
-      num_samples INT64, 
-      mean_latency_msec FLOAT64, 
-      meadian_latency_msec FLOAT64, 
-      standard_dev_msec FLOAT64, 
-      percentile_latency_0 FLOAT64, 
-      percentile_latency_20 FLOAT64, 
-      percentile_latency_50 FLOAT64, 
-      percentile_latency_90 FLOAT64, 
-      percentile_latency_95 FLOAT64, 
-      percentile_latency_98 FLOAT64, 
-      percentile_latency_99 FLOAT64, 
-      percentile_latency_99pt5 FLOAT64, 
-      percentile_latency_99pt9 FLOAT64, 
-      percentile_latency_100 FLOAT64, 
-      cpu_utilization_peak_percentage FLOAT64, 
-      cpu_utilization_mean_percentage FLOAT64, 
-      memory_utilization FLOAT64, 
-      FOREIGN KEY(configuration_id) REFERENCES performance_metrics.experiment_configuration (configuration_id) NOT ENFORCED
-    ) OPTIONS (description = 'Table for storing PERSISTENT DISK metrics extracted from listing benchmark tests');
-"""
+
 results = client.query(query_create_table_experiment_configuration)
 print(results)
 results = client.query(query_create_table_fio_metrics)
 print(results)
 results = client.query(query_create_table_vm_metrics)
 print(results)
-results = client.query(query_create_table_ls_metrics_gcsfuse)
-print(results)
-results = client.query(query_create_table_ls_metrics_persistent_disk)
+results = client.query(query_create_table_ls_metrics)
 print(results)
 
 query_insert_configuration = """
-    INSERT INTO gcsfuse-intern-project-2023.performance_metrics.experiment_configuration VALUES ('--implicit-dirs --max-conns-per-host 100 --enable-storage-client-library --debug_fuse --debug_gcs --log-file $LOG_FILE --log-format \"text\" --stackdriver-export-interval=30s', 'master', 'TIMESTAMP("2023-12-25 05:30:00+00")', 'ruchikasharmaa-testing-vm-1');
+    INSERT INTO gcsfuse-intern-project-2023.performance_metrics.experiment_configuration VALUES (1, '--implicit-dirs --max-conns-per-host 100 --enable-storage-client-library --debug_fuse --debug_gcs --log-file $LOG_FILE --log-format \"text\" --stackdriver-export-interval=30s', 'master', 'TIMESTAMP("2023-12-25 05:30:00+00")');
 """
 results = client.query(query_insert_configuration)
 print(results)
--- perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
+++ perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
@@ -46,18 +46,17 @@ sudo dpkg -i $HOME/release/packages/gcsfuse_${GCSFUSE_VERSION}_amd64.deb
 # Mounting gcs bucket
 cd "./perfmetrics/scripts/"
 LOG_FILE=gcsfuse-logs.txt
-GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100 --enable-storage-client-library --debug_fuse --debug_gcs --log-file $LOG_FILE --log-format \"text\" --stackdriver-export-interval=30s"
+GCSFUSE_FLAGS_PERIODIC_PERF_TESTS="--implicit-dirs --max-conns-per-host 100 --enable-storage-client-library --debug_fuse --debug_gcs --log-file $LOG_FILE --log-format \"text\" --stackdriver-export-interval=30s"
 
-echo "$GCSFUSE_FLAGS"
+echo "$GCSFUSE_FLAGS_PERIODIC_PERF_TESTS"
 ## Executing perf tests
 chmod +x run_load_test_and_fetch_metrics.sh
-#./run_load_test_and_fetch_metrics.sh "$GCSFUSE_FLAGS_PERIODIC_PERF_TESTS" "$BRANCH" "$END_DATE"
+./run_load_test_and_fetch_metrics.sh "$GCSFUSE_FLAGS_PERIODIC_PERF_TESTS" "$BRANCH" "$END_DATE"
 
 LOG_FILE=gcsfuse-list-tests-logs.txt
-GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100 --enable-storage-client-library --debug_fuse --debug_gcs --log-file $LOG_FILE --log-format \"text\" --stackdriver-export-interval=30s"
-echo "$GCSFUSE_FLAGS"
-# GCSFUSE_FLAGS_LIST_TESTS="--implicit-dirs --max-conns-per-host 100 --enable-storage-client-library --debug_fuse --debug_gcs --log-file $LOG_FILE --log-format \"text\" --stackdriver-export-interval=30s"
+GCSFUSE_FLAGS_LIST_TESTS="--implicit-dirs --max-conns-per-host 100 --enable-storage-client-library --debug_fuse --debug_gcs --log-file $LOG_FILE --log-format \"text\" --stackdriver-export-interval=30s"
+echo "$GCSFUSE_FLAGS_LIST_TESTS"
 # ls_metrics test. This test does gcsfuse mount first and then do the testing.
 cd "./ls_metrics"
 chmod +x run_ls_benchmark.sh
-#./run_ls_benchmark.sh "$GCSFUSE_FLAGS_LIST_TESTS" "$BRANCH" "$END_DATE"
+./run_ls_benchmark.sh "$GCSFUSE_FLAGS_LIST_TESTS" "$BRANCH" "$END_DATE"
