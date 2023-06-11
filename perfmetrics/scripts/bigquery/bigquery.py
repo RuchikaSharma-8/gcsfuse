9940e9609cdf5904e925150409ccc9668aefc242
RuchikaSharma-8 <2020csb1119@iitrpr.ac.in>
2023-06-11 15:27:11 +0000

    Testing

--- perfmetrics/scripts/bigquery/bigquery.py
+++ perfmetrics/scripts/bigquery/bigquery.py
@@ -203,8 +203,20 @@ def write_fio_metrics_to_bigquery(gcsfuse_flags, branch, end_date, values_all_jo
       AND end_date = '{}'
   """.format(gcsfuse_flags, branch, end_date)
   result = client.query(query_get_configuration_id)
-  config_id = result.result().fetchone().configuration_id
+  for row in result.result:
+    config_id = row.configuration_id
+
   print(config_id)
+
+  for values in values_all_jobs:
+    query_insert_into_fio_metrics = """
+      INSERT INTO gcsfuse-intern-project-2023.performance_metrics.fio_metrics
+      (configuration_id, test_type, num_threads, file_size_kb, start_time, end_time, iops, bandwidth_bytes_per_sec, IO_bytes, 
+        min_latency, max_latency, mean_latency, percentile_latency_20, percentile_latency_50, percentile_latency_90, percentile_latency_95)
+      VALUES('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}') 
+    """.format(config_id, values[0], values[1], values[2], values[3], values[4], values[5],  values[6], values[7],
+               values[8], values[9], values[10], values[11], values[12], values[13], values[14])
+
 
   for values in values_all_jobs:
     query_insert_into_fio_metrics = """
