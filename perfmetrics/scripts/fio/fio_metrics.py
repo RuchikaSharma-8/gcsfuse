89f4aeea4a5a7c0d28ec7ce111ee53704dced7c6
RuchikaSharma-8 <93065377+RuchikaSharma-8@users.noreply.github.com>
2023-06-13 11:50:01 +0530

    Update fio_metrics.py

--- perfmetrics/scripts/fio/fio_metrics.py
+++ perfmetrics/scripts/fio/fio_metrics.py
@@ -435,7 +435,7 @@ class FioMetrics:
 
     print(values)
 
-    bigquery.write_fio_metrics_to_bigquery(gcsfuse_flags, branch, end_date, values)
+    #bigquery.write_fio_metrics_to_bigquery(gcsfuse_flags, branch, end_date, values)
     #gsheet.write_to_google_sheet(worksheet_name, values)
 
   def get_metrics(self, filepath, gcsfuse_flags, branch, end_date, worksheet_name=None) -> List[Dict[str, Any]]:
