fe7a1d85d58a7108a68cd0456e69c8ee47756bf9
RuchikaSharma-8 <2020csb1119@iitrpr.ac.in>
2023-06-21 07:34:10 +0000

    Minor changes

--- perfmetrics/scripts/bigquery/bigquery_get_config.py
+++ perfmetrics/scripts/bigquery/bigquery_get_config.py
@@ -1,3 +1,31 @@
+# Copyright 2023 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Python script for getting the experiment configuration ID.
+
+This python script calls the bigquery module with the experiment details and gets the configuration ID
+
+Typical usage example from bigquery folder:
+  $ python3 bigquery_get_config.py [-h] [--gcsfuse_flags GCSFUSE_FLAGS] [--branch BRANCH] [--end_date END_DATE] [--config_name CONFIG_NAME]
+
+  Flag -h: Typical help interface of the script.
+  Flag gcsfuse_flags (required: str): Set of flags the gcsfuse flags used for experiment.
+  Flag branch (required: str): GCSFuse repo branch used for building GCSFuse.
+  Flag end_date (required: timestamp): Date till when experiments of this configuration are run.
+  Flag config_name (required: str): Name of the experiment configuration.
+
+Note: BigQuery API should be enabled for the project
+"""
 import bigquery
 import bigquery_setup
 import argparse
@@ -49,3 +77,4 @@ if __name__ == '__main__':
   args = parse_arguments(argv)
   bigquery_obj = bigquery.ExperimentsGCSFuseBQ(bigquery_setup.PROJECT_ID, bigquery_setup.DATASET_ID)
   exp_config_id = bigquery_obj.get_experiment_configuration_id(args.gcsfuse_flags[0], args.branch[0], args.end_date[0], args.config_name[0])
+  print(exp_config_id)
