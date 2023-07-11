6ad0c28c5ff3c6db0536a6be950be89ccd590f9f
RuchikaSharma-8 <2020csb1119@iitrpr.ac.in>
2023-07-11 06:30:36 +0000

    Extracting peak and mean ram utilization

--- perfmetrics/scripts/vm_metrics/vm_metrics.py
+++ perfmetrics/scripts/vm_metrics/vm_metrics.py
@@ -36,6 +36,7 @@ RECEIVED_BYTES_COUNT_METRIC_TYPE = 'compute.googleapis.com/instance/network/rece
 OPS_LATENCY_METRIC_TYPE = 'custom.googleapis.com/gcsfuse/fs/ops_latency'
 READ_BYTES_COUNT_METRIC_TYPE = 'custom.googleapis.com/gcsfuse/gcs/read_bytes_count'
 OPS_ERROR_COUNT_METRIC_TYPE = 'custom.googleapis.com/gcsfuse/fs/ops_error_count'
+RAM_UTI_METRIC_TYPE = 'agent.googleapis.com/memory/percent_used'
 
 @dataclasses.dataclass
 class MetricPoint:
@@ -80,9 +81,14 @@ OPS_ERROR_COUNT = Metric(
     reducer='REDUCE_SUM',
     group_fields=['metric.labels'])
 
+RAM_UTI_PEAK = Metric(
+    metric_type=RAM_UTI_METRIC_TYPE, factor=1, aligner='ALIGN_MAX')
+RAM_UTI_MEAN = Metric(
+    metric_type=RAM_UTI_METRIC_TYPE, factor=1, aligner='ALIGN_MEAN')
+
 METRICS_LIST = [
     CPU_UTI_PEAK, CPU_UTI_MEAN, REC_BYTES_PEAK, REC_BYTES_MEAN,
-    READ_BYTES_COUNT, OPS_ERROR_COUNT
+    READ_BYTES_COUNT, OPS_ERROR_COUNT, RAM_UTI_PEAK, RAM_UTI_MEAN
 ]
 
 # List of VM metrics extracted for listing tests
@@ -132,7 +138,11 @@ def _get_metric_filter(type, metric_type, instance, extra_filter):
         'metric.type = "{metric_type}" AND metric.labels.opencensus_task = '
         'ends_with("{instance_name}")').format(
             metric_type=metric_type, instance_name=instance)
-
+  elif (type == 'agent'):
+    metric_filter = (
+        'metric.type = "{metric_type}" AND metric.label.state = '
+        'ends_with("used")').format(
+        metric_type=metric_type)
   if (extra_filter == ''):
     return metric_filter
   return '{} AND {}'.format(metric_filter, extra_filter)
@@ -209,6 +219,9 @@ class VmMetrics:
                                          instance, metric.extra_filter)
     elif (metric.metric_type[0:6] == 'custom'):
       metric_filter = _get_metric_filter('custom', metric.metric_type, instance,
+                                         metric.extra_filter)
+    elif (metric.metric_type[0:5] == 'agent'):
+      metric_filter = _get_metric_filter('agent', metric.metric_type, instance,
                                          metric.extra_filter)
     else:
       raise Exception('Unhandled metric type')
